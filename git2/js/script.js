a542347e653e41519f45afd28f6b5b4a33d21dfa
bbbame <bamss9275@naver.com>
2023-12-27 11:53:26 +0900

    ㅇ

---
+++ git2/js/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    showSlides();
+}
+function showSlides() {
+    let imgSlide = document.getElementById('slideShow');
+    let images =imgSlide.getElementsByTagName('a');
+    setInterval(() => {
+
+    })
+}
