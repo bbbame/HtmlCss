547ab876db4686f1836665c3094e4e048b8cb88b
bbbame <bamss9275@naver.com>
2023-12-27 12:21:48 +0900

    ㄴ

--- git2/js/script.js
+++ git2/js/script.js
@@ -1,10 +1,17 @@
 window.onload = function() {
+    current = 0;
     showSlides();
 }
 function showSlides() {
-    let imgSlide = document.getElementById('slideShow');
-    let images =imgSlide.getElementsByTagName('a');
-    setInterval(() => {
-
-    })
+let imgSlide = document.querySelector('.imgSlide');
+let img = imgSlide.getElementsByTagName('a');
+for(let i=0 ; i < img.length ; i++) {
+    img[i].style.display = 'none'; //이미지 전부 감춤
+}
+current++;
+if(current > img.length) { //3보다 크면 1로 돌아감
+    current = 1;
+}
+img[current - 1].style.display='block';
+setTimeout(showSlides, 3000);
 }
